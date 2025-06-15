4fe37a29772be1c3238185f3a4699f73cf36957c
florjanPaja <florjanpaja0@gmail.com>
2025-06-15 10:44:46 +0200

    first html

--- frontend/utils/ui.js
+++ frontend/utils/ui.js
@@ -1,15 +1,23 @@
+// frontend/utils/ui.js
 export function loadHeaderFooter() {
-  .html(`
+  $('#header').html(`
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">Artigianato Online</a>
+        <a class="navbar-brand" href="/index.html">Artigianato Online</a>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a class="nav-link" href="/catalogo.html">Catalogo</a></li>
+            <li class="nav-item"><a class="nav-link" href="/carrello.html">Carrello</a></li>
+            <li class="nav-item"><a class="nav-link" href="/login.html">Login</a></li>
+          </ul>
+        </div>
       </div>
     </nav>
   `);
 
-  .html(`
-    <div class="container">
-      &copy; 2025 Artigianato Online - Tutti i diritti riservati
+  $('#footer').html(`
+    <div class="container text-center py-3 text-white bg-dark">
+      &copy; 2025 Artigianato Online
     </div>
   `);
 }
